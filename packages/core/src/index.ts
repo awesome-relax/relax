724e7e91b16b2b7c028573edef780e04f4c90e33
spencer <pallyoung@163.com>
2025-07-13 17:34:58 +0800

    feat: export event

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,4 @@
 export { selector } from './selector';
 export { atom, update } from './atom';
-export { State, type RelaxStateGetter, effect, removeEffect, dispose } from './state';
+export { State, type RelaxStateGetter, effect, removeEffect, dispose, get } from './state';
+export { createEvent } from './event';
